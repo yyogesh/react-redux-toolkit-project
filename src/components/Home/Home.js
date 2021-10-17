56d84224cd1b050c5448ce9fe38753ae14fb4aee
yyogesh <yogeshggn1@gmail.com>
2021-10-17 16:34:26 +0530

    createslice, action an reducer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,21 @@ import React, { useEffect } from 'react'
 import MovieListing from '../MovieListing/MovieListing';
 import movieApi from '../../common/apis/movieApi';
 import { APIKEY } from '../../common/apis/MovieApiKey';
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
+
 
 const Home = () => {
+    const dispatch = useDispatch();
+
     useEffect(() => {
         const fetchMovie = async () => {
-            const resposne = await movieApi.get(`?apikey=${APIKEY}&s=Matrix`);
-            console.log(resposne);
+            const response = await movieApi.get(`?apikey=${APIKEY}&s=Matrix`)
+                .catch((e) => {
+                    console.log('error', e);
+                });
+            dispatch(addMovies(response.data));
+            console.log(response.data);
         }
         fetchMovie();
     }, [])
--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { getAllMovies } from '../../features/movies/movieSlice';
+import MovieCard from '../MovieCard/MovieCard';
+import './MovieList.scss'
 
 const MovieListing = () => {
+    const movies = useSelector(getAllMovies);
+    console.log(movies);
+
+    const renderMovies = movies.Response === 'True' ? (
+        movies.Search.map((movie, index) => <MovieCard key={index} data={movie} />)
+    ) : <div className="movie-error">
+        <h3>{movies.Error
+        }</h3>
+    </div>
+
     return (
-        <div>
-            MovieListing
+        <div className="movie-wrapper">
+            <div className="movie-list">
+                <h2>Movies</h2>
+                <div className="movie-container">
+                    {renderMovies}
+                </div>
+            </div>
         </div>
     )
 }
